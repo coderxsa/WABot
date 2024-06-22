fa50d0e9dce22f1b781ddb963248f3818e8e0e0c
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-06-22 12:26:13 +0200

    Update script.js

--- INVITE/script.js
+++ INVITE/script.js
@@ -7,16 +7,26 @@ document.getElementById('botForm').addEventListener('submit', function(event) {
     const spam = document.getElementById('spam').value;
     const activity = document.getElementById('activity').value;
     const admin = document.getElementById('admin').value;
+    const language = document.getElementById('language').value;
+    const features = Array.from(document.getElementById('features').selectedOptions).map(option => option.value).join(', ');
+    const groupType = document.getElementById('groupType').value;
+    const communication = document.getElementById('communication').value;
+    const admins = document.getElementById('admins').value;
     const groupLink = document.getElementById('groupLink').value;
 
-    if (members === 'yes' && active === 'yes' && runtime == 'agree' && spam === 'agree' && activity === 'agree' && admin === 'yes' && groupLink) {
+    if (members === 'yes' && active === 'yes' && runtime === 'agree' && spam === 'agree' && activity === 'agree' && admin === 'yes' && groupLink) {
         const message = `
-More than 10 members: ${members}\n
+More than 15 members: ${members}\n
 Is the Group Active: ${active}\n
-Bot Runtime 2pm : 10pm?: ${runtime}\n
+Bot Runtime 1:30pm : 10pm?: ${runtime}\n
 Spam agreement: ${spam}\n
 Activity agreement: ${activity}\n
 Bot Admin status: ${admin}\n
+Primary Language: ${language}\n
+Bot Features: ${features}\n
+Group Type: ${groupType}\n
+Communication Frequency: ${communication}\n
+Number of Other Admins: ${admins}\n
 Group Link: ${groupLink}\n`;
         const encodedMessage = encodeURIComponent(message);
         const resultLink = `https://wa.me/27717311486?text=${encodedMessage}`;
